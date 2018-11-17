21445ed93fd3b9d04823881cbcda921d863fa2b3
Zekai <zkzhao33@gmail.com>
2018-11-17 14:59:37 -0800

    Create workspace

---
+++ Podfile
@@ -0,0 +1,9 @@
+
+platform :ios, '9.0'
+
+target 'Parker' do
+  use_frameworks!
+
+pod 'SVProgressHUD'
+
+end
---
+++ Pods/Target Support Files/Pods-Parker/Pods-Parker-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Parker : NSObject
+@end
+@implementation PodsDummy_Pods_Parker
+@end
