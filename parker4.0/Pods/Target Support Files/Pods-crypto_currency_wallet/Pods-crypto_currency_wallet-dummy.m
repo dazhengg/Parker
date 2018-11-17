d18bdde58497a171680ba873ddfd6e167488211d
Haoran Zhang <phrzhang@ucdavis.edu>
2018-11-17 14:41:36 -0800

    Delete 4.0

--- parker4.0/Pods/Target Support Files/Pods-crypto_currency_wallet/Pods-crypto_currency_wallet-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_crypto_currency_wallet : NSObject
-@end
-@implementation PodsDummy_Pods_crypto_currency_wallet
-@end
--- parker4.0/Pods/accountCell.swift
+++
@@ -1,8 +0,0 @@
-//
-//  accountCell.swift
-//  libPhoneNumber-iOS
-//
-//  Created by lihang pan on 2018/11/1.
-//
-
-import Foundation
