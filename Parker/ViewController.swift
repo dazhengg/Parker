c285a8a845cb7516181f379910c9e73903fef96e
Zekai <zkzhao33@gmail.com>
2018-11-16 22:29:37 -0800

    added map

---
+++ Parker/ViewController.swift
@@ -0,0 +1,39 @@
+import UIKit
+import MapKit
+
+class ViewController: UIViewController , CLLocationManagerDelegate{
+	
+	
+	var locationManager = CLLocationManager()
+	
+	@IBOutlet weak var map: MKMapView!
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+		self.locationManager.requestWhenInUseAuthorization()
+		
+		if CLLocationManager.locationServicesEnabled() {
+			
+			locationManager.delegate = self
+			locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+			locationManager.startUpdatingLocation()
+		}
+	}
+	
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+	
+	
+	func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+		
+		let locValue:CLLocationCoordinate2D = manager.location!.coordinate
+		print("locations = \(locValue.latitude) \(locValue.longitude)")
+		let userLocation = locations.last
+		let viewRegion = MKCoordinateRegion(center: (userLocation?.coordinate)!, latitudinalMeters: 600, longitudinalMeters: 600)
+		self.map.setRegion(viewRegion, animated: true)
+	}
+	
+}
