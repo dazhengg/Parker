a01c005790f41083b53ac826a6d77c44f8fae215
Zekai <zkzhao33@gmail.com>
2018-11-17 21:01:46 -0800

    solved conflict

--- Parker/CustomViewController/MapViewController.swift
+++ Parker/CustomViewController/MapViewController.swift
@@ -3,18 +3,20 @@ import MapKit
 
 class MapViewController: UIViewController , CLLocationManagerDelegate{
 	
-	
+	let newPin = MKPointAnnotation()
 	var locationManager = CLLocationManager()
+	var locatButtonPressed = false
 	
 	@IBOutlet weak var map: MKMapView!
 	
 	override func viewDidLoad() {
-		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
-		self.locationManager.requestWhenInUseAuthorization()
+			super.viewDidLoad()
+			// Do any additional setup after loading the view, typically from a nib.
+			self.locationManager.requestWhenInUseAuthorization()
 		
-		if CLLocationManager.locationServicesEnabled() {
+			if CLLocationManager.locationServicesEnabled() {
 			
+
 			locationManager.delegate = self
 			locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
 			locationManager.startUpdatingLocation()
@@ -27,7 +29,13 @@ class MapViewController: UIViewController , CLLocationManagerDelegate{
         let leftSwipe = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipe(sender:)))
         leftSwipe.direction = .left
         self.view.addGestureRecognizer(leftSwipe)
-	}
+
+					locationManager.delegate = self
+					locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+					locationManager.startUpdatingLocation()
+			}
+
+	
     @objc func handleSwipe(sender: UISwipeGestureRecognizer){
         if sender.state == .ended{
             switch sender.direction{
@@ -60,13 +68,25 @@ class MapViewController: UIViewController , CLLocationManagerDelegate{
 	}
 	
 	
+	
 	func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
 		
-		let locValue:CLLocationCoordinate2D = manager.location!.coordinate
-		print("locations = \(locValue.latitude) \(locValue.longitude)")
-		let userLocation = locations.last
-		let viewRegion = MKCoordinateRegion(center: (userLocation?.coordinate)!, latitudinalMeters: 600, longitudinalMeters: 600)
-		self.map.setRegion(viewRegion, animated: true)
+			let locValue:CLLocationCoordinate2D = manager.location!.coordinate
+			print("locations = \(locValue.latitude) \(locValue.longitude)")
+			let userLocation = locations.last! as CLLocation
+			let viewRegion = MKCoordinateRegion(center: userLocation.coordinate, latitudinalMeters: 600, longitudinalMeters: 600)
+			self.map.setRegion(viewRegion, animated: true)
+		
+
+	}
+	@IBAction func locateButton(_ sender: Any) {
+		locatButtonPressed = !locatButtonPressed
+		if(locatButtonPressed){
+			let userLocation = locationManager.location! as CLLocation
+			newPin.coordinate = userLocation.coordinate
+			map.addAnnotation(newPin)
+		}
+		
 	}
 	
 }
