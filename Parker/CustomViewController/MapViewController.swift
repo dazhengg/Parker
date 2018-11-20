1a13f03a618a91334d375d91aa86de9d2a4b1cea
Zekai <zkzhao33@gmail.com>
2018-11-19 18:27:37 -0800

    added custom pin

--- Parker/CustomViewController/MapViewController.swift
+++ Parker/CustomViewController/MapViewController.swift
@@ -1,9 +1,10 @@
 import UIKit
 import MapKit
 
-class MapViewController: UIViewController , CLLocationManagerDelegate{
+class MapViewController: UIViewController , CLLocationManagerDelegate, MKMapViewDelegate{
 	
 		let newPin = MKPointAnnotation()
+		let currentLocationPin = MKPointAnnotation()
 		var locationManager = CLLocationManager()
 		var locatButtonPressed = false
 	
@@ -71,10 +72,30 @@ class MapViewController: UIViewController , CLLocationManagerDelegate{
 		
 			let locValue:CLLocationCoordinate2D = manager.location!.coordinate
 			print("locations = \(locValue.latitude) \(locValue.longitude)")
-		//	let userLocation = locations.last! as CLLocation
+			let userLocation = locations.last! as CLLocation
 		//	let viewRegion = MKCoordinateRegion(center: userLocation.coordinate, latitudinalMeters: 600, longitudinalMeters: 600)
 		//	self.map.setRegion(viewRegion, animated: false)
+		
+			// Drop a pin at user's Current Location
+		
+			currentLocationPin.coordinate = CLLocationCoordinate2DMake(userLocation.coordinate.latitude, userLocation.coordinate.longitude);
+			currentLocationPin.title = "Current location"
+			self.map.addAnnotation(currentLocationPin)
+			self.map.delegate = self
 	}
+	
+	func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+		if annotation is MKUserLocation{
+			return nil
+		}
+		
+		let annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: "customannotation")
+		annotationView.image = UIImage(named: "round_near_me_black_24dp")
+		annotationView.canShowCallout = true
+	
+		return annotationView
+	}
+
 	
 	
 		@IBAction func locateButton(_ sender: Any) {
