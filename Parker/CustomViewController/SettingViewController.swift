94f72d64f713ccf1b25d283623b1356446c5c9f6
dazhengg <dazheng@ucdavis.edu>
2018-12-03 23:45:57 -0800

    Add files via upload

--- Parker/CustomViewController/SettingViewController.swift
+++ Parker/CustomViewController/SettingViewController.swift
@@ -8,19 +8,53 @@
 
 import UIKit
 
-class SettingViewController: UIViewController {
+class SettingViewController: UIViewController,UITextFieldDelegate {
 
+   
+    @IBOutlet weak var input_act: UITextField!
+    @IBOutlet weak var input_num: UITextField!
+    
+  
+    
     @IBOutlet weak var NavBar: UINavigationItem!
     @IBOutlet weak var BarButtonItem: UIBarButtonItem!
     override func viewDidLoad() {
         super.viewDidLoad()
         NavBar.title = "Setting"
         BarButtonItem.title = "Back"
+        input_act.delegate = self
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        if let x_act = UserDefaults.standard.object(forKey: "act_name") as? String {
+            input_act.text = x_act
+        }
+        if let y_num = UserDefaults.standard.object(forKey: "act_number") as? String {
+            input_num.text = y_num
+        }
+    }
+    
+    @IBAction func store_act(_ sender: Any) {
+       User.userName = input_act.text
+
+
+    }
+    @IBAction func store_num(_ sender: Any) {
+      User.number =  input_num.text
+        
+    }
+    
+    
     
     @IBAction func GoBackToMapVC(_ sender: Any) {
         //why my commits is not showing
         self.dismiss(animated: true, completion: nil)
     }
-    
+
+//    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+//        user.userName = input_act.text
+//        return false
+//
+//    }
+
 }
