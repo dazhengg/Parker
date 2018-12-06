2f33fc5f20b60d98776e34748235ec1fada7fdd2
dazhengg <dazheng@ucdavis.edu>
2018-12-05 21:19:35 -0800

    Briding_header for JSQMessage
    Messaging

---
+++ Bridging_Header.h
@@ -0,0 +1,13 @@
+//
+//  Bridging_Header.h
+//  Parker
+//
+//  Created by David Zheng on 12/4/18.
+//  Copyright © 2018 Zekai Zhao. All rights reserved.
+//
+
+#ifndef Bridging_Header_h
+#define Bridging_Header_h
+
+#import <JSQMessagesViewController/JSQMessages.h>
+#endif /* Bridging_Header_h */
